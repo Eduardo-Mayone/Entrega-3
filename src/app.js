102e8db539a54c729ec589c615a7944fc4f1712e
Eduardo Mayone <emayone@montecarlotv.com.uy>
2024-02-13 10:56:25 -0300

    Entrega 3

---
+++ src/app.js
@@ -0,0 +1,47 @@
+import ProductManager from "./productManager.js"
+import express from "express"
+let productos = new ProductManager("./src/Productos.json")
+const app = express()
+app.use(express.urlencoded({extended:true}))
+const error_id = {error: "No existe producto con ese ID"}
+const error_limit = {error: "Valor no valido de limit"}
+
+app.get("/products/:pid", async(req, res)=> {
+    try{
+        
+        let producto_por_id = await productos.getProductById(parseInt(req.params.pid))
+        res.send(producto_por_id)
+        console.log("este es el producto por id", producto_por_id)
+    } catch(e){
+        console.log(e)
+        res.send(error)
+    }        
+})
+
+app.get("/products/", async(req, res)=> {
+    try{
+        let productos_obtenidos
+        let cantidad = parseInt(req.query.limit)
+        if (cantidad != cantidad) { // o sea is NaN y no colocó limit o limit valido
+            productos_obtenidos = await productos.getProducts()
+            res.send(productos_obtenidos)
+            console.log("estos son los todos los productos sin limite válido", productos_obtenidos)
+        } else {
+            console.log("este es el limite", cantidad)
+            productos_obtenidos = await productos.getProducts()
+            if (cantidad == 0 || cantidad >= productos_obtenidos.length) {
+                console.log("El límite es 0 o mayor que los productos posibles")
+                res.send(error_limit)
+            } else {
+                let productos_limitados = (await productos.getProducts()).slice(0,cantidad)
+                console.log("estos son los productos limitados", productos_limitados)
+                res.send(productos_limitados)
+            }
+        }    
+    } catch(e){
+        console.log(e)
+    }
+         
+})
+
+app.listen(8080, ()=> console.log("servidor corriendo"))
---
+++ src/productManager.js
@@ -0,0 +1,106 @@
+import fs from 'fs';
+
+
+export default class ProductManager {
+    #url= "https://las_fotos.com/";
+    constructor(path) {
+        this.path = path;
+        console.log("Este es el path: ", this.path)
+        
+    }
+
+    getProducts = async() => {
+
+        const data = await fs.promises.readFile(this.path, 'utf-8');
+        const productos = JSON.parse(data);
+        //console.log("Lista de todos los productos: ", productos);
+
+        return productos
+    }
+
+    generarId = async() => {
+        let index = 0;
+        const productos = await this.getProducts();
+        
+        if (!productos.length){
+         return index = 1;
+        }
+        index = productos.pop().id +1;
+        console.log("Id generado: ", index)
+        return index;
+    }
+    
+    
+    addProduct = async(producto) => {
+        const productos = await this.getProducts();
+        console.log("🚀 ~ ProductManager ~ addProduct=async ~ productos:", productos)
+        
+        if (productos) {
+            producto.id = await this.generarId(); 
+            producto.thumbnail = this.#url + producto.thumbnail;
+        }
+        else {
+            producto.id = 1;
+            producto.thumbnail = this.#url + producto.thumbnail;
+        }
+        productos.push(producto);
+        console.log("Producto agregado: ", productos)
+        await fs.promises.writeFile(this.path, JSON.stringify(productos, null, "\t"));
+    }
+
+    
+    getProductById = async(id) => {
+        const productos = await this.getProducts();
+        const producto_buscado = productos.find ((producto) => producto.id === id);
+        if (!producto_buscado) {
+            console.log ("Product NOT FOUND");
+            throw (error);
+        }
+        else {
+            console.log("Producto buscado por id: ", producto_buscado);
+            return producto_buscado;  
+        }
+    }
+
+    updateProduct = async(producto_a_actualizar) => {
+
+        const producto_buscado = await this.getProductById(producto_a_actualizar.id);
+        if (producto_buscado) {
+            const productos = await this.getProducts();
+            let index = productos.findIndex((producto) => producto.id === producto_buscado.id)
+            for (let key in producto_a_actualizar) {
+                productos[index][key] = producto_a_actualizar[key];
+            }
+            console.log("Con producto actualizado: ", productos);
+            await fs.promises.writeFile(this.path, JSON.stringify(productos, null, "\t"));
+        }
+        else {
+            console.log ("Product NOT FOUND");
+            throw (error);
+        }
+    }
+
+    deleteProduct = async(id) => {
+        const nuevo_array = [];
+        const producto_buscado = await this.getProductById(id);
+        if (producto_buscado) {
+            const productos = await this.getProducts();
+            let index = productos.findIndex((producto) => producto.id === producto_buscado.id)
+            for (let i=0; i< productos.length; i++) {
+                if (i === index) {
+                    continue;
+                }
+                else {
+                    nuevo_array.push(productos[i]);
+                }
+            }
+            console.log("Con producto borrado: ", nuevo_array);
+            await fs.promises.writeFile(this.path, JSON.stringify(nuevo_array, null, "\t"));
+        }
+        else {
+            console.log ("Product NOT FOUND");
+            throw (error);
+        }
+    }
+
+}
